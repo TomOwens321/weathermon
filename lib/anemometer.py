22e3a48d62b0b1653b34cfb91510b5645e4dc6a1
TomOwens321 <tom@towens.com>
2020-07-24 12:03:43 -0600

    Simplify isr routine, sample wind for 10 seconds for each reading. (#2)

--- lib/anemometer.py
+++ lib/anemometer.py
@@ -1,5 +1,6 @@
 
 import time
+import threading
 import RPi.GPIO as GPIO
 
 class Anemometer():
@@ -9,10 +10,11 @@ class Anemometer():
     def __init__(self, pin=25):
         self.anemometer_pin = pin
         self.pulse_count = 0
+        self.speed = 0.0
+        self.max_gust = 0.0
         self.last_reading_time = time.time()
         self.last_time = 0
-        self.delta = 0
-        self.gust_delta = 0xffff
+        self.thread = threading.Thread(target=self.set_speeds)
 
         self.setup()
 
@@ -21,18 +23,18 @@ class Anemometer():
         GPIO.setup(self.anemometer_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     def _isr(self, channel):
-        now_time = time.time()
-        self.delta = now_time - self.last_time
+        self.pulse_count += 1
 
-        if self.delta > 0.008:
-            self.last_time = now_time
-            self.pulse_count += 1
-            if self.delta < self.gust_delta:
-                self.gust_delta = self.delta
-        # print("Anemometer trigger event")
+    def set_speeds(self):
+        while True:
+            self.speed = self.get_windspeed()
+            if self.speed > self.max_gust:
+                self.max_gust = self.speed
+            time.sleep(10)
 
     def start(self):
-        GPIO.add_event_detect(self.anemometer_pin, GPIO.RISING, callback=self._isr, bouncetime=30)
+        GPIO.add_event_detect(self.anemometer_pin, GPIO.RISING, callback=self._isr)
+        self.thread.start()
 
     def stop(self):
         GPIO.remove_event_detect(self.anemometer_pin)
@@ -44,13 +46,16 @@ class Anemometer():
             rpm = (self.pulse_count / (now_time - self.last_reading_time))  * 30
         return rpm
 
-    def windspeed(self):
+    def get_windspeed(self):
         now_time = time.time()
         time_delta = now_time - self.last_reading_time
         mph = (self.pulse_count / time_delta) * self.WIND_FACTOR
         self.pulse_count = 0
         self.last_reading_time = now_time
         return mph
+
+    def windspeed(self):
+        return self.speed
 
     def instant_windspeed(self):
         mph = 0.0
@@ -61,11 +66,8 @@ class Anemometer():
         return mph
 
     def gust(self):
-        mph = 0.0
-        if self.gust_delta < 10.0:
-            mph =  ( 1 / self.gust_delta ) * self.WIND_FACTOR
-        return mph
+        return self.max_gust
 
     def reset_gust(self):
-        self.gust_delta = 0xffff
+        self.max_gust = 0.0
 
