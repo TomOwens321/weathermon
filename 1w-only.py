004a4e36ad63f86037a2120f41b750b27ca1c01b
Tom Owens <tom@towens.com>
2020-12-12 04:34:47 -0700

    Start service isolation

--- 1w-only.py
+++ 1w-only.py
@@ -1,23 +1,70 @@
+#!/usr/bin/env python3
 """Can be added to weathermon main or run stand-alone"""
 import time
+import lib.conversions as conv
+import json
+from lib.mqtt import Mqtt
+from lib.influxdb import Influxdb
 from lib.ownet import Ownet
 
 HOSTS=['greenhousepi']
-SENSORS=[]
 
-for host in HOSTS:
-    client = Ownet(host)
+db = Influxdb(host='omv', port='8086')
+db_client = db.client()
 
-    for dev in client.devices:
-        device = {'host': host, 'device': dev, 'type': client.read(dev, 'type')}
-        SENSORS.append(device)
+mq = Mqtt('192.168.1.10')
+mq_topic = 'sun-chaser/testing'
 
-if SENSORS:
-    print("I found {} devices".format(len(SENSORS)))
-    print(SENSORS)
+def scan_1w_devices(hosts, sensors):
 
-client = Ownet(HOSTS[0])
-device = SENSORS[0]['device']
-while True:
-    print("Temperature: {:.2f}".format(client.read(device) * (9/5) + 32))
-    time.sleep(5)
+    for host in hosts:
+        client = Ownet(host)
+
+        for dev in client.devices:
+            device = {'host': host, 'device': dev, 'type': client.read(dev, 'type')}
+            sensors.append(device)
+
+    if sensors:
+        print("I found {} devices".format(len(sensors)))
+        print(sensors)
+
+    return sensors
+
+def get_1w_temperature(device):
+    reading = {}
+    client = Ownet(device['host'])
+    temp = client.read(device['device'])
+    if temp > 75.0:
+        return reading
+    name = client.device_name(device['device'])
+    location = 'Greenhouse'
+    if name == 'Test_Sensor2':
+        location = 'WellHouse'
+
+    reading['measurement'] = 'temperature'
+    reading['tags'] = {'sensorName': name, 'sensorLocation': location, 'sensorType': device['type']}
+    reading['fields'] = {
+        'tempc': temp,
+        'tempf': round(conv.c_to_f(temp), 2)
+    }
+
+    return reading
+
+def main():
+    while True:
+        data = []
+        sensors = []
+        sensors = scan_1w_devices(HOSTS, sensors)
+        for device in sensors:
+            measurement = get_1w_temperature(device)
+            print(measurement)
+            data.append(measurement)
+        db_client.write_points(data, database='weather_data', retention_policy='one_year')
+        for d in data:
+            topic = mq_topic + '/' + d['measurement'] + '/' + d['tags']['sensorName']
+            mq.send(topic, json.dumps(d))
+
+        time.sleep(60)
+
+if __name__ == '__main__':
+    main()
