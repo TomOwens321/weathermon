1da9e6fd820d55469e485ac324ae7854b3ce73a9
Tom Owens <tom@towens.com>
2020-12-13 13:18:32 -0700

    Add barometer readings

---
+++ baro_only.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import time
+import json
+import lib.conversions as conv
+from lib.lps22hb import LPS22HB
+
+MQTT_TOPIC = 'sun-chaser/weather'
+mq = Mqtt('192.168.1.10')
+
+db = Influxdb(host='rpi4b-1.ourhouse', port='8086')
+db_client = db.client()
+
+def get_pressure(sensor):
+    reading = {}
+    pr = sensor.pressure()
+    adjusted_pr = conv.adjust_for_altitude(pr)
+
+    reading['measurement'] = 'barometer'
+    reading['tags'] = {'sensorType': 'LPS22HB', 'sensorLocation': 'Wellhouse', 'sensorName': 'WH Pressure'}
+    reading['fields'] = {
+        'raw_hpa': pr,
+        'raw_inhg': conv.hpa_to_inhg(pr),
+        'adjusted_hpa': adjusted_pr,
+        'adjusted_inhg': conv.hpa_to_inhg(adjusted_pr),
+        'pressure': round(conv.hpa_to_inhg(adjusted_pr),2)
+    }
+
+    return reading
+
+def main():
+    pr = LPS22HB()
+
+    while True:
+        data = []
+        pressure = get_pressure(pr)
+        data.append(pressure)
+        db_client.write_points(data, database='weather_data', retention_policy='one_year')
+        for d in data:
+            topic = MQTT_TOPIC + '/' + d['measurement'] + '/' + d['tags']['sensorName']
+            mq.send(topic, json.dumps(d))
+
+        time.sleep(60)
+
+if __name__ == '__main__':
+    main()
