4d37760cb282a2dbd9c7eff3ac61521ff6e4cc0b
Tom Owens <tom@towens.com>
2020-12-13 06:01:37 -0700

    Add wind - step 1

---
+++ wind_only.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import time
+import json
+from lib.mqtt import Mqtt
+from lib.ad import ADS1015
+from lib.anemometer import Anemometer
+
+db = Influxdb(host='rpi4b-1.ourhouse', port='8086')
+client = db.client()
+
+MQTT_TOPIC = 'sun-chaser/weather'
+
+def get_wind(an, dr):
+    reading = {}
+    windspeed = an.windspeed()
+    d_text,d_value = dr.direction()
+    gust = an.gust()
+
+    reading['measurement'] = 'wind'
+    reading['tags'] = {'sensorName': 'Anemometer', 'sensorLocation': 'Wellhouse', 'sensorType': 'WeatherRack'}
+    reading['fields'] = {
+        'windspeedmph': windspeed,
+        'windgustmph': gust,
+        'winddirtext': d_text,
+        'winddir': d_value
+    }
+
+    return reading
+
+def main():
+    an = Anemometer(17)
+    dr = ADS1015()
+    mq = Mqtt('192.168.1.10')
+    data = []
+
+    data.append(get_wind(an, dr))
+    print(data)
+        db_client.write_points(data, database='weather_data', retention_policy='one_year')
+        for d in data:
+            topic = MQTT_TOPIC + '/' + d['measurement'] + '/' + d['tags']['sensorName']
+            mq.send(topic, json.dumps(d))
+
+        time.sleep(60)
+
+
+    pass
+
+if __name__ == '__main__':
+    main()
