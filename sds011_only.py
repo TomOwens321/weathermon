e4c7811624fb30a6ee0a772d0a7040abc1153927
Tom Owens <tom@towens.com>
2023-10-25 06:35:52 -0600

    Send sds011 data to MQTT

--- sds011_only.py
+++ sds011_only.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
-import time
+import json
 from sds011 import SDS011
+from lib.mqtt import Mqtt
 from lib.influxdb import Influxdb
 
 PORT = "/dev/ttyUSB0"
 RATE = 10
+MQTT_TOPIC = 'sun-chaser/weather'
+mq = Mqtt('rpi4b-1.ourhouse')
 
 sds = SDS011(port=PORT, rate=RATE, use_database=True)
 sds.set_working_period(rate=RATE)
@@ -29,12 +32,17 @@ def get_measurement():
     }
     return reading
 
-def run():    
+def run():
     while True:
         data = []
         reading = get_measurement()
         data.append(reading)
         db_client.write_points(data, database='weather_data', retention_policy='one_year')
+
+        for d in data:
+                topic = MQTT_TOPIC + '/' + d['measurement'] + '/' + d['tags']['sensorName']
+                mq.send(topic, json.dumps(d))
+
         print(reading)
 
 if __name__ == '__main__':
