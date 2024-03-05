107053a70631b6a60e22b89c9fc47b37ed53f955
TomOwens321 <tomowens321@gmail.com>
2024-03-05 05:56:55 -0700

    Adjustments and updates for the wellhouse anemometer (#23)

--- lib/mqtt.py
+++ lib/mqtt.py
@@ -1,11 +1,11 @@
 """ Sends data to mqtt server """
-from paho.mqtt.client import Client
+import paho.mqtt.client as mqtt
 
 class Mqtt(object):
 
     def __init__(self, host):
         self.host = host
-        self.client = Client(client_id="weathermon", protocol=5)
+        self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client_id="weathermon", protocol=5)
 
     def send(self, topic, payload):
         try:
