7dd54bc14592d95037dd91e4766df0511d6ef9e8
Tom Owens <tom@towens.com>
2024-03-11 06:24:07 -0600

    Get corrected values for mq135 sensor

--- lib/conversions.py
+++ lib/conversions.py
@@ -3,6 +3,19 @@
 MBAR_CONV = 33.8639
 ALTITUDE = 4837
 
+CORA = .00035
+CORB = .02718
+CORC = 1.39538
+CORD = .0018
+CORE = -.003333333
+CORF = -.001923077
+CORG = 1.130128205
+
+PARA = 116.6020682
+PARB = 2.769034857
+
+RZERO = 40.98
+
 def c_to_f(tempc):
     return (tempc * (9/5)) + 32
 
@@ -13,3 +26,14 @@ def adjust_for_altitude(reading, alt=ALTITUDE):
 def hpa_to_inhg(hpa):
     return hpa / MBAR_CONV
 
+def get_correction_factor(temperature, humidity):
+    if temperature < 20:
+        return CORA * temperature * temperature - CORB * temperature + CORC - (humidity - 33.0) * CORD
+    else:
+        return CORE * temperature + CORF * humidity + CORG
+
+def get_corrected_resistance(temperature, humidity, resistance):
+    return resistance / get_correction_factor(temperature, humidity)
+
+def get_corrected_pm(temperature, humidity, resistance):
+    return PARA * pow((get_corrected_resistance(temperature, humidity, resistance) / RZERO), -PARB)
--- mq135_only.py
+++ mq135_only.py
@@ -3,6 +3,7 @@
 import serial as ser
 import time
 import json
+import lib.conversions as conv
 from datetime import datetime
 from lib.mqtt import Mqtt
 from lib.influxdb import Influxdb
@@ -11,7 +12,14 @@ db = Influxdb(host='knode.ourhouse', port='8086')
 db_client = db.client()
 
 MQTT_TOPIC = 'sun-chaser/weather'
+MQTT_TEMPERATURE_TOPIC = MQTT_TOPIC + '/temperature/Ambient_Outdoor'
+MQTT_HUMIDITY_TOPIC = MQTT_TOPIC + '/humidity/Ambient_Outdoor'
+
 mq = Mqtt('rpi4b-1.ourhouse')
+mq_listen = Mqtt('rpi4b-1.ourhouse', client_id='weather_listener')
+
+temperature = 20.0
+humidity = 50.0
 
 def send_data(sensorReading):
     data = []
@@ -32,7 +40,6 @@ def send_data(sensorReading):
             mq.send(topic, json.dumps(d))
 
 def run(ser):
-
     start = time.time()
     maxPPM = 0.0
     while True:
@@ -40,7 +47,10 @@ def run(ser):
         line = ser.readline().decode('utf-8').strip()
 
         try:
-            ppm = float(line.split(':')[7])
+            resistance = float(line.split(':')[5])
+            # ppm = float(line.split(':')[7])
+            print(f'Temperature: {temperature} | Humidity {humidity} | Resistance: {resistance}')
+            ppm = conv.get_corrected_pm(temperature, humidity, resistance)
         except:
             print(f'Error: {line}')
             continue
@@ -55,8 +65,34 @@ def run(ser):
             maxPPM = 0.0
             start = time.time()
 
+def start_mqtt_listener():
+    mq_listen.client.connect('rpi4b-1.ourhouse')
+    mq_listen.client.on_connect = on_connect
+    mq_listen.client.on_message = on_message
+    mq_listen.client.loop_start()
+
+def on_connect(client, userdata, flags, rc, properties=None):
+    print(f'Connected with result code {rc}')
+    if rc == 0:
+        print(f'Subscribing to {MQTT_TEMPERATURE_TOPIC}')
+        print(f'Subscribing to {MQTT_HUMIDITY_TOPIC}')
+        client.subscribe([(MQTT_TEMPERATURE_TOPIC, 0), (MQTT_HUMIDITY_TOPIC, 0)])
+    else:
+        print('Connection failed')
+
+def on_message(client, userdata, message):
+    print(f'{message.topic}: {message.payload}')
+    msg = json.loads(message.payload)
+    if message.topic == MQTT_TEMPERATURE_TOPIC:
+        temperature = float(msg['fields']['tempc'])
+    elif message.topic == MQTT_HUMIDITY_TOPIC:
+        humidity = float(msg['fields']['humidity'])
+    else:
+        print(f'Unknown topic: {message.topic}')
+        return
 
 if __name__ == '__main__':
+    start_mqtt_listener()
     ser = ser.Serial('/dev/ttyUSB0', 9600)
     ser.timeout = 10
     ser.stopbits = 1
